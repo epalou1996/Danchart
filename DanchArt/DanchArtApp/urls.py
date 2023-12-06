c2ab59fadcad1106433d8904a1bf519be50c5006
epalou1996 <eduardopalou96@gmail.com>
2023-12-06 22:12:39 +0100

    Cambios de fuentes y estilos

--- DanchArt/DanchArtApp/urls.py
+++ DanchArt/DanchArtApp/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
 
     path('', views.home, name='home' ),
     path('shop/', views.shop , name='shop'),
+    path('galeria/', views.galeria , name='gallery'),
+    path('theartist/', views.theartist , name='theartist'),
+    path('faq/', views.faq , name='faq'),
+    path('proceso/', views.proceso , name='proceso'),
     
 
 ]
--- DanchArt/DanchArtApp/views.py
+++ DanchArt/DanchArtApp/views.py
@@ -7,9 +7,25 @@ import datetime
 
 def home(request):
     fecha_actual = datetime.datetime.now()
-    return render(request, 'DanchArtApp/home.html')
+    return render(request, 'DanchArtApp/home3.html')
 
 def shop(request):
     fecha_actual = datetime.datetime.now()
     return render(request, 'DanchArtApp/shop.html')
+
+def galeria(request):
+    fecha_actual = datetime.datetime.now()
+    return render(request, 'DanchArtApp/gallery.html')
+
+def faq(request):
+    fecha_actual = datetime.datetime.now()
+    return render(request, 'DanchArtApp/faq.html')
+
+def proceso(request):
+    fecha_actual = datetime.datetime.now()
+    return render(request, 'DanchArtApp/proceso.html')
+
+def theartist(request):
+    fecha_actual = datetime.datetime.now()
+    return render(request, 'DanchArtApp/theartist.html')
 
